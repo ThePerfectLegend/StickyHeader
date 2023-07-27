02401525b4a9c972c9e7ee7731e8b1be3c580c7c
Nizami Tagiyev <94032706+ThePerfectLegend@users.noreply.github.com>
2023-07-27 16:14:57 +0500

    Initial Commit

---
+++ StickyHeader/StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderApp.swift
+//  StickyHeader
+//
+//  Created by Nizami Tagiyev on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
